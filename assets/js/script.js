16326998a7bee1709053b4f52104c9aace77a4a7
sellolintang <sellolintang242@gmail.com>
2025-05-03 21:51:34 +0700

    JS, Rapihin Halaman Web, Add Footer

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+let currentValue = '';
+let isMeterToCm = true;
+
+function valuecal(val) {
+  currentValue += val;
+  document.getElementById('inputMeter').value = currentValue;
+}
+
+function clearall() {
+  currentValue = '';
+  document.getElementById('inputMeter').value = '';
+  document.getElementById('resultCm').value = '';
+}
+
+function convertToCm() {
+  const input = parseFloat(currentValue);
+  if (isNaN(input)) {
+    document.getElementById('resultCm').value = 'Input tidak valid';
+    return;
+  }
+
+  let result;
+  if (isMeterToCm) {
+    result = input * 100;
+    document.getElementById('resultCm').value = result + ' cm';
+  } else {
+    result = input / 100;
+    document.getElementById('resultCm').value = result + ' m';
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const flipButton = document.querySelector('.button-flip');
+    const introTitle = document.querySelector('.intro h2');
+    const introText = document.querySelector('.intro p');
+    const inputField = document.getElementById('inputMeter');
+    const resultField = document.getElementById('resultCm');
+    const mainTitle = document.getElementById('main-title');
+
+    flipButton.addEventListener('click', () => {
+        isMeterToCm = !isMeterToCm;
+        if (isMeterToCm) {
+          document.title = 'M => CM CONVERTER';
+            mainTitle.textContent = 'Meter(M) => Sentimeter(CM) CONVERTER';
+            flipButton.textContent = 'CM => M';
+            introTitle.textContent = 'M => CM';
+            inputField.placeholder = 'Masukkan Angka M';
+            resultField.placeholder = 'Hasil (Cm)';
+        } else {
+          document.title = 'CM => M CONVERTER';
+            mainTitle.textContent = 'Sentimeter(CM) => Meter(M) CONVERTER';
+            flipButton.textContent = 'M => CM';
+            introTitle.textContent = 'CM => M';
+            inputField.placeholder = 'Masukkan Angka CM';
+            resultField.placeholder = 'Hasil (M)';
+        }
+        clearall();
+    });
+});
